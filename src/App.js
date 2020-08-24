a96a9d33c61e7f7f3cb78e837b8be786df83eeaa
jamy11 <jamyarrahman82@gmail.com>
2020-08-25 02:38:38 +0600

    problem faced data is not showing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
+const App = () => {
 
+  const APP_ID = "087d32f8";
+  const APP_KEY = "9795f7e20454bce8709d1a5c54470258";
+
+  
+
+  useEffect( () =>{
+    console.log('effect has been running');
+  },[]);
+
+  const getRecipes = async() =>{
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json()
+    console.log(data)
+  }
+
+
+
+  return (
+    <div className = "App">
+      
+      <form className="search-form">
+        <input className="search-bar" type="text"></input>
+        <button  className="search-button" type="submit">Search</button>
+      </form>
+      
+    </div>
+
+  );
+};
 
 export default App;
